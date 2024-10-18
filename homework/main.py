e3019c0728f0c2f6afb7a61e2a103cf539ac6545
kingHyen <modan8199@gmail.com>
2024-10-18 17:34:43 +0900

    - commit

--- homework/main.py
+++ homework/main.py
@@ -1,30 +1,36 @@
-from fastapi import FastAPI , HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from typing import Literal
 
 app = FastAPI()
 
+
 class calculating_machine(BaseModel):
-    sign : str 
-    num1 : int
-    num2 : int
+    sign: Literal["+", "-", "*", "%", "^", "//"]
+    num1: int = Field(...,description='숫자를 입력해주세요')
+    num2: int = Field(...,description='숫자를 입력해주세요')
+
 
 def calculate(sign: str, num1: int, num2: int):
-    if  sign == '+':
-        return num1 + num2
-    elif sign == '-':
-        return num1 - num2
-    elif sign == '*':
-        return num1 * num2
-    elif sign == '%':
-        return num1 % num2
+    if sign == "+":
+        return {"+": num1 + num2}
+    elif sign == "-":
+        return {"-": num1 - num2}
+    elif sign == "*":
+        return {"*": num1 * num2}
+    elif sign == "%":
+        return {"%": num1 % num2}
+    elif sign == "//":  # 몫과 나머지 연사
+        return {"몫": num1 // num2, "나머지": num1 % num2}
+    elif sign == "^":  # 거듭제곱 연산
+        return {"^": num1**num2}
     else:
-        raise HTTPException(status_code=400,detail='입력 읒 같이하지마셈!!!!!')
+        raise HTTPException(status_code=400, detail="잘 못 입력하셨습니다.")
 
-@app.post('/')
-async def post_T(request:calculating_machine):
-    
-    sum = calculate(request.sign,request.num1,request.num2)
-    
+
+@app.post("/")
+async def post_T(request: calculating_machine):
+
+    sum = calculate(request.sign, request.num1, request.num2)
+
     return sum
-
-
